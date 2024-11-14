50e50f40830ce56e5e5c34c030c30347cebfb69c
Joenel <joenelsevellejo831@gmail.com>
2024-11-14 14:23:49 +0800

    added changes

--- app/socket-io/custom/Usesocket.ts
+++ app/socket-io/custom/Usesocket.ts
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react'
 import { Socket, io } from 'socket.io-client'
 
-const temporaryURL = 'https://web-socket-backend-beryl.vercel.app/' //add this to env later
+const temporaryURL = 'wss://web-socket-backend-beryl.vercel.app/'
 
 const Usesocket = () => {
     const [ Socket, setSocket ] = useState<Socket | null>(null)
 
     useEffect(()=> {
-        const newSocket = io(temporaryURL, { transports: ['websocket'] })
+        const newSocket = io(temporaryURL, 
+            { transports: ['websocket'], secure: true }
+        )
         setSocket(newSocket)
-        
+        console.log(temporaryURL)
         return () => {
             newSocket.disconnect()
         }
